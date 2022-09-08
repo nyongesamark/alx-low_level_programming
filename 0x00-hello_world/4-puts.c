f72bea815824bf59f74a040800f55487fdbec332
nyongesamark <nyongesamark13@gmail.com>
2022-09-08 07:31:14 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Description: main - prints out the string in stdio.
+ * Return: 0 if successful.
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
