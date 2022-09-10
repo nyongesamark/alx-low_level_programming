af882350ed1c82ca14194d688bb12e84637efcab
nyongesamark <nyongesamark13@gmail.com>
2022-09-10 06:55:08 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers and letters
+ * Return:0
+ */
+
+int main(void)
+{
+	int num = 48;
+	int lw = 'a';
+
+	while (num >= 57)
+	{
+		putchar(num);
+		num += 1;
+	}
+	while (lw >= 'f')
+	{
+		putchar(lw);
+		lw += 1;
+	}
+	putchar('\n');
+	return (0);
+}
