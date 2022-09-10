4a20385ad1f177b604e4c017732f00990aac06f6
nyongesamark <nyongesamark13@gmail.com>
2022-09-10 06:27:19 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints number 0- 9
+ * Return:0
+ */
+
+int main(void)
+{
+	int num = 48;
+
+	while (num <= 57)
+	{
+		putchar(num);
+		num += 1;
+	}
+	putchar('\n');
+	return (0);
+}
