feb76abb498f1ced674f66188a573f66a5226a83
nyongesamark <nyongesamark13@gmail.com>
2022-09-10 03:51:09 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers 1-9
+ * Return:0
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+	return (0);
+}
