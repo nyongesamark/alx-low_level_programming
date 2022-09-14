9fdab9ef2a4db52ca57b259261948c773cc57054
nyongesamark <nyongesamark13@gmail.com>
2022-09-14 14:06:12 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#inlude "main.h"
+
+/**
+ * add - Adds two numbers
+ * @num1: the first integer
+ * @num2: the second integer
+ *
+ * Return: the result
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
